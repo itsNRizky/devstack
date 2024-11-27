26b2e6ffb0dd9bb9d1d62a1ce67d81d618c05cb4
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2024-11-28 00:03:34 +0700

    Update tailwinds and upload assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-white">Novian Rizky Perdana</h1>
+    <>
+      <h1 className="h1-bold">Novian Rizky Perdana</h1>
+      <h1 className="h1-bold font-space-grotesk">Novian Rizky Perdana</h1>
+    </>
   );
 }
